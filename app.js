e85f00fb1e3fcbb8b8b541fae8b254600dfe3d8b
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2022-12-20 20:14:45 +0530

    Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
-
+const saltRounds = 10;
 
 
 const app = express();
@@ -42,10 +42,12 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
 
     const userName = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+
+    bcrypt.hash(password, saltRounds, function(err, hash){
       const newUser = new User({
         email : userName,
-        password : password
+        password : hash
       });
 
       newUser.save(function(err){
@@ -55,23 +57,26 @@ app.post("/register", function(req, res){
           res.render('secrets');
         }
       });
+    });
 });
 
 
 app.post("/login", function(req, res){
   const userName = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email : userName}, function(err, foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-          if(foundUser.password === password){
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(result){
             res.render('secrets');
           }
-      }
+      });
     }
+  }
   });
 });
 
